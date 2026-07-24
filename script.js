50fb699ab5b830a808e67512e6de0849528ccfd1
DinoXanh <mumcip99@gmail.com>
2026-07-24 09:18:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,61 @@
-const danhSach = [];
-for (let i = 1; i <= 50; i++) {
-    danhSach.push("Mục " + i);
-}
+const danhSach = [
+    "Tiger I",
+    "Tiger II (King Tiger)",
+    "Panther",
+    "Panzer IV",
+    "Panzer III",
+    "StuG III",
+    "Jagdpanther",
+    "JagdTiger",
+    "Elefant",
+    "Maus",
+    "IS-1",
+    "IS-2",
+    "KV-1",
+    "KV-2",
+    "T-34",
+    "T-34-85",
+    "SU-85",
+    "SU-100",
+    "SU-152",
+    "ISU-152",
+    "M4 Sherman",
+    "M4A3E8 Easy Eight",
+    "M26 Pershing",
+    "M3 Lee",
+    "M10 Wolverine",
+    "M18 Hellcat",
+    "M36 Jackson",
+    "Churchill",
+    "Cromwell",
+    "Comet",
+    "Matilda II",
+    "Valentine",
+    "Crusader",
+    "A30 Challenger",
+    "Char B1",
+    "Somua S35",
+    "Renault R35",
+    "Hotchkiss H39",
+    "Type 95 Ha-Go",
+    "Type 97 Chi-Ha",
+    "Type 1 Chi-He",
+    "Type 3 Chi-Nu",
+    "Semovente 75/18",
+    "M13/40",
+    "Turán I",
+    "Turán II",
+    "Hetzer",
+    "LT vz.38",
+    "Toldi I",
+    "38M Nimród"
+];
 
 const soMucMoiTrang = 10;
 const tongTrang = Math.ceil(danhSach.length / soMucMoiTrang);
 
 function hienThiTrang(trang) {
+
     const list = document.getElementById("list");
     list.innerHTML = "";
 
@@ -19,9 +68,9 @@ function hienThiTrang(trang) {
         list.appendChild(li);
     }
 
-    const buttons = document.querySelectorAll("#pagination button");
-    buttons.forEach(btn => btn.classList.remove("active"));
-    buttons[trang - 1].classList.add("active");
+    const tatCaNut = document.querySelectorAll("#pagination button");
+    tatCaNut.forEach(btn => btn.classList.remove("active"));
+    tatCaNut[trang - 1].classList.add("active");
 }
 
 const pagination = document.getElementById("pagination");
@@ -30,9 +79,9 @@ for (let i = 1; i <= tongTrang; i++) {
     const btn = document.createElement("button");
     btn.textContent = i;
 
-    btn.addEventListener("click", function () {
+    btn.onclick = function () {
         hienThiTrang(i);
-    });
+    };
 
     pagination.appendChild(btn);
 }
